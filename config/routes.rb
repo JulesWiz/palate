96b8770fbd5693459f3beda894e846fe08a6ee8a
JulesWiz <ngjulie0203@gmail.com>
2014-04-17 13:45:11 +0800

    Created 2 controller testing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Palate::Application.routes.draw do
   scope :api do
     get '' => "api#index", as: :api, defaults: {format: :json}
 
-
     resources :users, except: [ :edit, :new ], defaults: {format: :json} do
       get ':id' => "users#show", on: :collection
     end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,12 +4,25 @@ describe UsersController do
 
   render_views
 
+  before :all do
+    User.create(email: 'test@example.com', password: '123', password_confirmation: '123')
+  end
+
   describe 'GET index' do
     it 'returns list of users' do
       get :index, :format => :json
       expect(response.status).to eq 200
       expect(response.body).to include 'test@example.com'
-      expect(JSON.load(response.body)["users"][1]["email"]).to eq 'test@example.com'
+      expect(JSON.load(response.body)["users"][0]["email"]).to eq 'test@example.com'
+    end
+  end
+
+  describe 'GET show' do
+    it 'returns one user' do
+      get :show, :id => 1, :format => :json
+      expect(response.status).to eq 200
+      expect(response.body).to include 'test@example.com'
+      expect(JSON.load(response.body)["email"]).to eq 'test@example.com'
     end
   end
 end
